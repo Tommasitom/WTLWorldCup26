7e2a5a46650fd7fdd64d0c3f74276147e531d55d
Tommasitom <Tommy@mc-enery.com>
2026-06-15 01:11:52 +0100

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "WTLWorldCup26",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0 }}>
+        {children}
+      </body>
+    </html>
+  );
+}
