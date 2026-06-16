a7b876730acd59c0d1a799defccfb284f9500891
Tommasitom <Tommy@mc-enery.com>
2026-06-16 16:43:58 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -130,7 +130,7 @@ export default function Home() {
     <div style={styles.page}>
       <div style={styles.header}>
 
-        <img src="/wtl-logo.png" style={styles.logo} />
+        <img src="/public-wtl-logo.png.png" style={styles.logo} />
 
         <p style={styles.subtitle}>Live leaderboard & match results</p>
 
