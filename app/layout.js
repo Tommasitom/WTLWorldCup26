3ba0a1a09d43fec011c7816a088b28682da9bc81
Tommasitom <Tommy@mc-enery.com>
2026-06-21 02:50:41 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,15 @@
+
 export const metadata = {
   title: "WTLWorldCup26",
+  openGraph: {
+    title: "WTL World Cup 26",
+    description: "Weekly World Cup sweepstakes podcast & leaderboard",
+    images: [
+      {
+        url: "https://wtl-world-cup26.vercel.app/wtl.logo.png",
+      }
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
